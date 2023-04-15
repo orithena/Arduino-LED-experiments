fc019fcf7f7da754cd513a8b970d6ef29c9eeb65
orithena <dave@fsinfo.cs.tu-dortmund.de>
2023-04-15 15:28:06 +0200

    interrupt_test: better dump format

--- interrupt_test/int_dump/int_dump.ino
+++ interrupt_test/int_dump/int_dump.ino
@@ -1,34 +1,42 @@
-#define PIN D3
+#define PIN D6
 volatile int state = 0;
 int oldstate = 0;
 volatile uint32_t micro = 0;
+volatile uint32_t ms = 0;
 
 
 void setup() {
     delay(2000);
     Serial.begin(230400);
-    pinMode(PIN, INPUT);
+    pinMode(PIN, INPUT_PULLUP);
     Serial.println(" ");
     Serial.print(digitalRead(PIN));
     Serial.print(" ");
     Serial.println(micros());
-    attachInterrupt(PIN, dump, FALLING);
+    attachInterrupt(PIN, falling, FALLING);
+//    attachInterrupt(PIN, rising, RISING);
 }
 
 void loop() {
   if( state > 0 ) {
-    Serial.print(digitalRead(PIN));
     Serial.print("\t");
-    Serial.print(state);
+    Serial.print(state == 1 ? "FALL" : "RISE");
     Serial.print("\t");
-    Serial.println(micro);
+    Serial.print(micro);
+    Serial.print("\t");
+    Serial.println(ms);
     oldstate = state;
   }
   state = 0;
 }
 
-void dump() {
-//  state = digitalRead(PIN);
+ICACHE_RAM_ATTR void falling() {
   state = 1;
+  micro = micros();
+  ms = millis();
+}
+
+ICACHE_RAM_ATTR void rising() {
+  state = 2;
   micro = micros();
 }
